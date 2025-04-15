fac60df1f5480dbd8a9429fbb2eafe29b2541c36
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-04-15 16:38:12 -0300

    FEAT: Teste da lista usando v-for

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,34 @@
 <script setup>
-
+  const jogosAcao = [
+    {
+      id: 1,
+      nome: 'Bioshock 1',
+      plataforma: 'Ps, Xbox, Pc',
+      preco: 25,
+      capa: ''
+    },
+    {
+      id: 2,
+      nome: 'The last of us 2 Remastered',
+      plataforma: 'Ps5, Pc',
+      preco: 400,
+      capa: ''
+    },
+    {
+      id: 3,
+      nome: 'Dead by daylihght',
+      plataforma: 'Ps, Xbox, Pc',
+      preco: 59.99,
+      capa: ''
+    },
+    {
+      id: 4,
+      nome: 'Dying light',
+      plataforma: 'Ps, Xbox, Pc',
+      preco: 39.99,
+      capa: ''
+    },
+  ]
 </script>
 
 
@@ -64,6 +93,11 @@
     </ul>
   </div>
 
+  <ul>
+    <li v-for="jogo in jogosAcao">
+      nome: {{ jogo.nome }}
+    </li>
+  </ul>
 
 
   </header>
