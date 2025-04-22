3cfa5b3f9b985988d5c59c9ee630a4e792e80c75
rodolphorocha <rodolpho.r2008@gmail.com>
2025-04-22 11:03:49 -0300

    FEAT: adição das imagens

--- src/App.vue
+++ src/App.vue
@@ -64,56 +64,56 @@
       nome: 'Outlast',
       plataforma: 'Ps, Xbox, Pc',
       preco: 59.99,
-      capa: ''
+      capa: 'public/img/outlast.jpg'
     },
     {
       id: 2,
       nome: 'Fnaf',
       plataforma: 'Ps5, Pc',
       preco: 75,
-      capa: ''
+      capa: 'public/img/FNAF.jpg'
     },
     {
       id: 3,
       nome: 'Visage',
       plataforma: 'Ps, Xbox, Pc',
       preco: 65.99,
-      capa: ''
+      capa: 'public/img/visage.jpg'
     },
     {
       id: 4,
       nome: 'Phasmophobia',
       plataforma: 'Pc',
       preco: 59.99,
-      capa: ''
+      capa: 'public/img/phasmophobia.jpg'
     },
     {
       id: 5,
       nome: 'Poppy Playtime',
       plataforma: 'Pc',
       preco: 46.99,
-      capa: ''
+      capa: 'public/img/poppyplaytime.jpg'
     },
     {
       id: 6,
       nome: 'Stay out of the house',
       plataforma: 'Pc',
       preco: 59.90,
-      capa: ''
+      capa: 'public/img/stayoutofthehouse.jpg'
     },
     {
       id: 7,
       nome: 'Nun Massacre',
       plataforma: 'Pc',
       preco: 50,
-      capa: ''
+      capa: 'public/img/nunmassacre.jpg'
     },
     {
       id: 8,
       nome: 'Silent Hill',
       plataforma: 'Ps, Xbox, Pc',
       preco: 349.90,
-      capa: ''
+      capa: 'public/img/silenthill.jpg'
     },
   ]
 
@@ -123,56 +123,56 @@
       nome: 'Minecraft',
       plataforma: 'Ps, Xbox, Pc',
       preco: 200,
-      capa: ''
+      capa: 'public/img/minecraft.png'
     },
     {
       id: 2,
       nome: 'ARK',
       plataforma: 'Ps5, Xbox, Pc',
       preco: 46.99,
-      capa: ''
+      capa: 'public/img/ark.jpg'
     },
     {
       id: 3,
       nome: 'The Forest',
       plataforma: 'Ps, Xbox, Pc',
       preco: 37.99,
-      capa: ''
+      capa: 'public/img/theforest.jpg'
     },
     {
       id: 4,
       nome: 'No man`s sky',
       plataforma: 'Ps, Xbox, Pc',
       preco: 162,
-      capa: ''
+      capa: 'public/img/nomanssky.jpg'
     },
     {
       id: 5,
       nome: 'Stranded Deep',
       plataforma: 'Ps, Xbox, Pc',
       preco: 37.99,
-      capa: ''
+      capa: 'public/img/strandeddeep.jpg'
     },
     {
       id: 6,
       nome: 'Subnautica',
       plataforma: 'Ps, Xbox, Pc',
       preco: 124.50,
-      capa: ''
+      capa: 'public/img/subnautica.png'
     },
     {
       id: 7,
       nome: 'Raft',
       plataforma: 'Pc',
       preco: 36.99,
-      capa: ''
+      capa: 'public/img/raft.jpg'
     },
     {
       id: 8,
       nome: 'Green Hell',
       plataforma: 'Ps, Xbox, Pc',
       preco: 16.82,
-      capa: ''
+      capa: 'public/img/greenhell.jpg'
     },
   ]
 
@@ -182,56 +182,56 @@
       nome: 'Red Dead Redemption',
       plataforma: 'Ps, Xbox, Pc',
       preco: 200,
-      capa: ''
+      capa: 'public/img/rdr2.png'
     },
     {
       id: 2,
       nome: 'GTA',
       plataforma: 'Ps5, Xbox, Pc',
       preco: 37,
-      capa: ''
+      capa: 'public/img/gta.png'
     },
     {
       id: 3,
       nome: 'Watch Dogs',
       plataforma: 'Ps, Xbox, Pc',
       preco: 22.49,
-      capa: ''
+      capa: 'public/img/watchdogs2.png'
     },
     {
       id: 4,
       nome: 'Assassin`s Creed',
       plataforma: 'Ps, Xbox, Pc',
       preco: 349.99,
-      capa: ''
+      capa: 'public/img/ac.jpg'
     },
     {
       id: 5,
       nome: 'Spider Man',
       plataforma: 'Ps, Pc',
       preco: 249.90,
-      capa: ''
+      capa: 'public/img/spiderman.jpg'
     },
     {
       id: 6,
       nome: 'Batman Arkham',
       plataforma: 'Ps, Xbox, Pc',
       preco: 89.99,
-      capa: ''
+      capa: 'public/img/batman.jpg'
     },
     {
       id: 7,
       nome: 'DAYZ',
       plataforma: 'Ps, Xbox, Pc',
       preco: 74.99,
-      capa: ''
+      capa: 'public/img/dayz.jpg'
     },
     {
       id: 8,
       nome: 'Fallout',
       plataforma: 'Ps, Xbox, Pc',
       preco: 59.99,
-      capa: ''
+      capa: 'public/img/fallout.jpg'
     },
   ]
 </script>
